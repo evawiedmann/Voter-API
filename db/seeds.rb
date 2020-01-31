c1954a4b546f96071e14e50900d21a64a36a7760
Eva Wiedmann <evawiedmann@gmail.com>
2020-01-31 15:26:57 -0800

    running into more errors, thinking about burning down again

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,26 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-class Seed
-
-  def self.begin
-    seed = Seed.new
-    seed.generate_states
+20.times do |i|
+  @state = State.create(name: Faker::Address.state,
+    check_reg: Faker::Verb.past_participle, register: Faker::Verb.base, absentee: Faker::GreekPhilosophers.quote, next_election: Faker::Construction.heavy_equipment, reg_deadlines: Faker::Space.distance_measurement)
+    @state.save
+    puts "Created #{State.count} States."
   end
-
-  def generate_states
-    20.times do |i|
-      state = State.create!(
-        name: Faker::Address.state,
-        check_reg: Faker::Verb.past_participle,
-        register: Faker::Verb.base,
-        absentee: Faker::GreekPhilosophers.quote,
-        next_election: Faker::Construction.heavy_equipment,
-        reg_deadlines: Faker::Space.distance_measurement
-      )
-      # puts "State #{i}: Author is #{state.name} and quotation is '#{state.check_reg}'."
-    end
-  end
-end
-
-Seed.begin
